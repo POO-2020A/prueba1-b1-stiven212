9ca794c2a8af9b7c6eed305c62b70ac023932bfb
Chalo Salvador <edwin.salvador@epn.edu.ec>
2020-08-06 10:51:23 -0500

    Update MixedCase.java

--- MixedCase.java
+++ MixedCase.java
@@ -26,7 +26,7 @@ public class MixedCase {
 
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-        System.out.print("In: ");
+        System.out.print("In:");
         String s = inp.nextLine();
         //write your code below
         for (int i = 0; i < s.length(); i++) {
